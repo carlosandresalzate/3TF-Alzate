1fad2f12b79ac0ac847e231cc63289f204e865d7
Carlos Andres Alzate <unarteman@gmail.com>
2025-02-07 20:20:49 -0300

    Agrego la logica y la platilla para generar una tabla que liste los cursos;

--- src/app/modules/dashboard/pages/courses/courses.component.ts
+++ src/app/modules/dashboard/pages/courses/courses.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+//
+import { CoursesService } from '../../../../core/services/courses.service';
+import { Course } from './models/course';
+//
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-courses',
   standalone: false,
@@ -8,7 +14,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './courses.component.scss',
 })
 export class CoursesComponent implements OnInit {
+  // Array de cursos que se muestran en la tabla
+  courses: Course[] = [];
+
+  // columnas que se mostraran en la tabla de angular material
+  displayedColumns: string[] = [
+    'edit',
+    'name',
+    'description',
+    'schedule',
+    'delete',
+  ];
+
+  constructor(
+    private coursesServices: CoursesService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  /**
+   * @description Metodo que se ejecuta al inicializar el componente.
+   * se suscribe al servicio para obtener la lista de cursos.
+   */
   ngOnInit(): void {
-    console.log('Hola mundo');
+    this.coursesServices.getCourses().subscribe((data) => {
+      this.courses = data;
+    });
   }
+
+  /**
+   * @description Muestra un snackbar con un mensaje
+   * @param {string} message - Mensaje a mostrar en el snackbar.
+   *
+   */
+  showSnackbar(message: string): void {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
+
+  // Metodo para editar
+
+  // TODO: falta agregar los metodos para editar y eliminar cursos. (¿Deberia hacer un utilidad para copartirla en diferentes modulos?)
 }
--- src/app/modules/dashboard/pages/courses/courses.module.ts
+++ src/app/modules/dashboard/pages/courses/courses.module.ts
@@ -4,14 +4,23 @@ import { CommonModule } from '@angular/common';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses.component';
 
+// pipes
+import { SharedModule } from '../../../../shared/shared.module';
+
+//
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    CoursesComponent
-  ],
+  declarations: [CoursesComponent],
   imports: [
     CommonModule,
-    CoursesRoutingModule
-  ]
+    CoursesRoutingModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    SharedModule,
+  ],
 })
-export class CoursesModule { }
+export class CoursesModule {}
