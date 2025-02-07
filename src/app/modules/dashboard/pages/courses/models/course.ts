2a3f763daf74136cca10fc4d6ec8f70b700537d5
Carlos Andres Alzate <unarteman@gmail.com>
2025-02-07 18:15:13 -0300

    Se crea la interfaz para modelar el curso

---
+++ src/app/modules/dashboard/pages/courses/models/course.ts
@@ -0,0 +1,10 @@
+/**
+ * @interface Course
+ * @description Define la estructura de un curso.
+ */
+export interface Course {
+  id: string;
+  name: string;
+  description: string;
+  schedule: string;
+}
