fc03fed18f468cc8896c0fa3759f4550d937a601
Carlos Andres Alzate <unarteman@gmail.com>
2025-02-09 14:30:39 -0300

    Creacion: modelo/interface para inscripciones;

---
+++ src/app/modules/dashboard/pages/enrollments/models/enrollments.ts
@@ -0,0 +1,10 @@
+/**
+ * @interface Inscription
+ * @description Define la estructura de una inscripcion
+ */
+export interface Enrollments {
+  id: string;
+  studentId: string;
+  courseId: string;
+  date: Date;
+}
