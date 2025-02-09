67f247f7f44c309229395d146e36cda857ae7d96
Carlos Andres Alzate <unarteman@gmail.com>
2025-02-09 19:28:17 -0300

    Se agrega la interaccion basica al modulo, tabla, botones y titulo

--- src/app/modules/dashboard/pages/enrollments/enrollments.component.ts
+++ src/app/modules/dashboard/pages/enrollments/enrollments.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Enrollments } from './models/enrollments';
+import { EnrollmentsService } from '../../../../core/services/enrollments.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
+/**
+ * @description Componente que muestra el listado de las inscripciones
+ */
 @Component({
   selector: 'app-enrollments',
   standalone: false,
-  
-  templateUrl: './enrollments.component.html',
-  styleUrl: './enrollments.component.scss'
-})
-export class EnrollmentsComponent {
 
+  templateUrl: './enrollments.component.html',
+  styleUrl: './enrollments.component.scss',
+})
+export class EnrollmentsComponent implements OnInit {
+  /**
+   * Array de inscripciones para mostrar la tabla
+   */
+  enrollments: Enrollments[] = [];
+
+  displayedColumns: string[] = ['edit', 'student', 'course', 'date', 'delete'];
+
+  constructor(
+    private enrollmentsService: EnrollmentsService,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar
+  ) {}
+
+  /**
+   * @description se suscribe al servicio para obtener la lista actualizada de inscripciones
+   */
+  ngOnInit(): void {
+    this.enrollmentsService.getEnrollments().subscribe((data) => {
+      this.enrollments = data;
+    });
+  }
+
+  /**
+   * @description Muestra un snackbar con el mensaje indicado.
+   * @param {string} message - mensaje que se va a mostrar
+   */
+  showSnackbar(message: string): void {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
 }
--- src/app/modules/dashboard/pages/enrollments/enrollments.module.ts
+++ src/app/modules/dashboard/pages/enrollments/enrollments.module.ts
@@ -4,8 +4,21 @@ import { CommonModule } from '@angular/common';
 import { EnrollmentsRoutingModule } from './enrollments-routing.module';
 import { EnrollmentsComponent } from './enrollments.component';
 
+import { SharedModule } from '../../../../shared/shared.module';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
 @NgModule({
   declarations: [EnrollmentsComponent],
-  imports: [CommonModule, EnrollmentsRoutingModule],
+  imports: [
+    CommonModule,
+    EnrollmentsRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    SharedModule,
+  ],
 })
 export class EnrollmentsModule {}
